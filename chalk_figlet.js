08a81518ffed335f5fc4fb619b8413c1e3f56447
Iti singh <106474251+Itisingh3@users.noreply.github.com>
2023-02-08 17:23:35 +0530

    Update and rename index.js to chalk_figlet.js

---
+++ chalk_figlet.js
@@ -0,0 +1,18 @@
+// figlet
+//  This method allows you to create ASCII Art from text. It takes in 3 parameters: Input Text - A string of text to turn into ASCII Art.
+let figlet=require("figlet");
+console.log(figlet.textSync('Iti!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
+
+
+
+
+// chalk
+// Chalk module in Node.js is the third-party module that is used for styling the format of text and allows us to create our own themes in the node.js project.
+const chalk = require('chalk');
+console.log(chalk.blue('Hello Iti!')); 
